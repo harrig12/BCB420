0e58c623ca24e98ef06aec41b0e8d862ef327f28
Cait <cait.harrigan@mail.utoronto.ca>
2018-02-15 21:21:39 -0500

    HUGO multimapper used to reduce unmapped genes

---
+++ HUGOmapperParsing.R
@@ -0,0 +1,19 @@
+#script to handle HUGO multisymbol mapper output
+
+#write these to a file to use other mapping techniques to match them to HUGO symbols
+#those with approved symbols not in HUGOsymbols should be removed
+#those which are withdrawn should be removed
+#those wich are unmatched should be removed and manually searched
+#those with synonyms or previous symbols should be placed into the synMap
+
+#read in mappings and update synMap
+HUGOmap <- read.delim("symHUGOmultiMapper.txt", header = T, sep="\t",stringsAsFactors = F)
+unmatched <- HUGOmap[(HUGOmap$Match.type == "Unmatched"), c("Input")]
+mappable <- HUGOmap[(HUGOmap$Match.type == "Previous symbol"| HUGOmap$Match.type =="Synonyms"),
+                     c("Input", "Approved.symbol")]
+colnames(mappable) <- c("symbols", "synonyms")
+synMap <- rbind(synMap, mappable)
+
+
+
+
--- expressionDataDownload.R
+++ expressionDataDownload.R
@@ -17,19 +17,23 @@ if (! require(GEOquery, quietly=TRUE)) {
   library(GEOquery)
 }
 
+
+
 # Work with RNAseq seems much easier by comparison: we already get unique gene
 # Gene.IDs.
 
 # download the normalized, annotated dataset from GEO
 #
-fName <- "GSE84712_TPM_all_samples.txt"
-library(readr)
-GSE84712 <- read_tsv(fName)
-str(GSE84712)
-save(GSE84712, file="/GSE84712.RData")
+# fName <- "GSE84712_TPM_all_samples.txt"
+# library(readr)
+# GSE84712 <- read_tsv(fName)
+# str(GSE84712)
+# save(GSE84712, file="/GSE84712.RData")
 
+setwd("..")
 load("GSE84712.RData")
 load("zu/inst/extdata/HUGOsymbols.RData")
+load("zu/inst/extdata/synMap.RData")
 
 nrow(GSE84712) # 19097 rows
 any(is.na(GSE84712$Gene.ID))       # FALSE
@@ -37,54 +41,94 @@ any(GSE84712$Gene.ID == "")        # FALSE
 any(grepl("/", GSE84712$Gene.ID))  # FALSE
 any(duplicated(GSE84712$Gene.ID))  # FALSE
 
+#start by summarizing the data we're looking at
+(colnames(GSE84712))
+#controls are columns 3-29, experiment conditions are columns 30-81
+GSE84712$controlMean <- rowMeans(GSE84712[,3:29])
+GSE84712$treatedMean <- rowMeans(GSE84712[,30:81])
+head(GSE84712[,c("controlMean", "treatedMean")], 20)
+
 sum(HUGOsymbols %in% GSE84712$Gene.ID) # 17628: 92 %
 
 missingRNAseqGene.IDs <- HUGOsymbols[!(HUGOsymbols %in% GSE84712$Gene.ID)]
 
 # how many of these can be recovered, because they are aliases?
-
 extraRNAseqGene.IDs <- GSE84712$Gene.ID[!(GSE84712$Gene.ID %in% HUGOsymbols)]
-# what are these?
-cat(sprintf("# [%d] %s\n", 1:20, head(extraRNAseqGene.IDs, 20)))
-# [1] A1BG-AS1       -  antisense
-# [2] A2MP1          -  pseudogene
-# [3] AA06           -  ???
-# [4] AAA1           -  synonym for NPSR1-AS1
-# [5] AACSP1
-# [6] AATK-AS1
-# [7] ABCA11P
-# [8] ABCA17P
-# [9] ABCC13         - pseudogene
-# [10] ABCC6P1
-# [11] ABCC6P2
-# [12] ABHD14A-ACY1  - readthrough (NMD candidate)
-# [13] ABP1          - NEW SYMBOL: AOC1
-# [14] ACN9          - NEW SYMBOL: SDHAF3
-# [15] ACPL2         - NEW SYMBOL: PXYLP1
-# [16] ACPT          - NEW SYMBOL: ACP4
-# [17] ACRC          - NEW SYMBOL: GCNA
-# [18] ACTR3BP2
-# [19] ACTR3BP5
-# [20] ADAM21P1
 
-load("zu/inst/extdata/synMap.RData")
+#synMap acts as a lookup table
 str(synMap)
-# mappable:
-x <- extraRNAseqGene.IDs[extraRNAseqGene.IDs %in% synMap$synonyms] # 1105
 
-# recover 1105 Gene.IDs will raise coverage
+# mappable
+mappable <- extraRNAseqGene.IDs[extraRNAseqGene.IDs %in% synMap$synonyms]
+length(mappable) # 1105
+
+# recovering 1105 Gene.IDs will raise coverage
 # to 17628 + 1105 = 18733 ... 98 %
+
+#which couldn't be mapped?
+unmappable <- extraRNAseqGene.IDs[!(extraRNAseqGene.IDs %in% mappable)]
+write(unmappable, file = "symUnmapped.txt", ncolumns = 1)
+
+#update symMap with HUGO symbols that could be found
+source("HUGOmapperParsing.R")
+mappable <- extraRNAseqGene.IDs[extraRNAseqGene.IDs %in% synMap$synonyms]
+
+#now that we have more mappings, there are some duplicates.
+#(Two subunits mapping to the same HUGO symbol for example)
+wDuplicates <- GSE84712
+wDuplicates$reMappedID <- wDuplicates$Gene.ID
+
+#if a gene is mappable, rename it in wDuplicates
+for (x in mappable){
+  wDuplicates$reMappedID[wDuplicates$Gene.ID == x] <- synMap$symbols[synMap$synonyms == x]
+}
+
+#find which HUGO symbols are represented more than once in the experiment
+#first find the duplicated geneIDs
+dupHUGOs <- wDuplicates$reMappedID[which(duplicated(wDuplicates$reMappedID))]
+(length(dupHUGOs)) #31
+
+#then find original versions of those geneIDs
+doubleMappedGenes <- wDuplicates[, c("Gene.ID","reMappedID")][which(wDuplicates$reMappedID %in% doubleMapped),]
+(nrow(doubleMappedGenes)) # 60, two of the doubleMapped were actually triple mapped
+
+#Show original and new mappings, manually decide which expression values should be used
+#as the values for that HUGO symbol. In class we decided we will keep only probe with
+#highest mean/median with robust statistics. This translates similarly for RNAseq.
+(doubleMappedGenes)
+#print the average expression value beside each
+
 
 # value distributions:
 
-boxplot(GSE84712[ , 2:7],
+cyclicPalette <- colorRampPalette(c("#00AAFF",
+                                    "#DDDD00",
+                                    "#FFAA00",
+                                    "#00AAFF",
+                                    "#DDDD00",
+                                    "#FFAA00",
+                                    "#00AAFF"))
+
+boxplot(GSE84712[ , 3:29],
         boxwex = 0.6,
         notch = TRUE,
-        main = "GSE84712",
+        main = "GSE84712 controls",
         outline = FALSE,
-        col = c(rep(myArrayCols[5], 3), rep(myArrayCols[17], 3)))
+        col = cyclicPalette(27))
 
-# etc ...
+boxplot(GSE84712[ , 30:55],
+        boxwex = 0.6,
+        notch = TRUE,
+        main = "GSE84712 Lead30",
+        outline = FALSE,
+        col = cyclicPalette(25))
+
+boxplot(GSE84712[ , 56:81],
+        boxwex = 0.6,
+        notch = TRUE,
+        main = "GSE84712 Lead3",
+        outline = FALSE,
+        col = cyclicPalette(25))
 
 
 # [END]
