10ffbb3aeb2230fd34017988e35ac9a62bf9f746
Cait <cait.harrigan@mail.utoronto.ca>
2018-03-11 12:44:28 -0400

    overwrite w/o asking

--- scripts/PGPmart.sh
+++ scripts/PGPmart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Warning: files may be overwiten silently"
+echo "Warning: files may be overwiten without asking"
 read -p "Continue (y/n)? " choice
 case "$choice" in 
   y|Y ) for fn in data/PGP/*;
 	do
 	echo "unzipping sample ${fn}"
-	unzip ${fn} -d data/PGP 
+	unzip -o ${fn} -d data/PGP 
 	done;;
   n|N ) echo "did nothing";;
   * ) echo "invalid";;
