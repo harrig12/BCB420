445d01bde11e9fc865f6356dcf5305241825fb94
Cait <cait.harrigan@mail.utoronto.ca>
2018-03-11 20:11:08 -0400

    chr20 parse update and interval plotting

--- personalVariant.R
+++ personalVariant.R
@@ -8,10 +8,10 @@
 #
 # Purpose: Explore variant density in and out of protein-coding genes
 #
-# Versions:
+# Versions: 1.1 Stream with chr20 parsing scripts
 #           1.0 Initial exploration
 #
-# Notes: Associated with personal genomes unit (2.3) in my BCB420 journal
+# Notes: Associated with personal genomes unit (2.1) in my BCB420 journal
 # http://steipe.biochemistry.utoronto.ca/abc/students/index.php/User:Caitlin_Harrigan/Journal420
 #
 # ==============================================================================
@@ -21,14 +21,23 @@ if (!require(vcfR, quietly=TRUE)) {
   library(vcfR)
 }
 
+if (!require(readr, quietly=TRUE)) {
+  install.packages("readr")
+  library(readr)
+}
+
+if (!require(intervals, quietly=TRUE)) {
+  install.packages("intervals")
+  library(intervals)
+}
+
+CHRLEN20 <- 64444167
+CH20CENTROMERE <- c(26369569, 29369569)
+
 # load in vcf information
-par001 <- read.vcfR("../data/PGP/PGPC_0001_S1.flt.vcf.gz")
-
-# subset for just chromosome 20
-sel <- getCHROM(par001) == "chr20"
-
-#create chromosome object
-CH20_001 <- create.chromR(par001[sel], "Ch20_001")
+load("../data/PGP/chr20_001.Rdata")
+CH20_001 <- CH20
+rm(CH20)
 
 #lets get to know our chromosome
 show(CH20_001)
@@ -39,7 +48,43 @@ head(variant.table(CH20_001))
 #we can still look at the quality measure
 chromoqc(CH20_001)
 
+#read in ensemble data (download details documented in journal)
+tmp <- read_tsv("../data/HGNC_chr20.txt")
 
+#check for uniqness in the HGNC symbol
+sum(duplicated(tmp$`HGNC symbol`))
+
+#subset as appropriate
+Chr20GeneData <- data.frame(sym = tmp$`HGNC symbol`,
+                            start = tmp$`Gene start (bp)`,
+                            end = tmp$`Gene end (bp)`,
+                            stringsAsFactors = FALSE)
+
+#check all elements have coordinates on the chromosome (0 <= coord <= CHRLEN20)
+#genes
+(max(Chr20GeneData$end) <= CHRLEN20)
+#variants
+(max(CH20_001@var.info$POS) <= CHRLEN20)
+
+#quick plot
+geneIntervals <- Intervals(Chr20GeneData[, c("start", "end")])
+rownames(geneIntervals) <- Chr20GeneData$sym
+
+plot(geneIntervals,
+     use_points = F,
+     xlab = "Coordinate",
+     ylab = "n Overlaps",
+     xlim = c(0,CHRLEN20),
+     ylim = c(0,2),
+     lwd = 5,
+     use_names = F,
+     main = "Chromosome 20 participant 001 genes",
+     axes = T)
+axis(2, at = c(0,1,2))
+
+#draw centromoere
+rect(CH20CENTROMERE[1], -0.05, CH20CENTROMERE[2], 2.05, col = "#ff00007F", border = NA)
+legend(x = 4e+07, y = 0.5, c("HGNC gene", "Centromere"), col=c(1,"#ff00007F"), lwd=3)
 
 
 # [END]
