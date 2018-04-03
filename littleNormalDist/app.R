1f75aefa852740ef57ea10791187f377fd737802
Cait <cait.harrigan@mail.utoronto.ca>
2018-04-03 18:39:08 -0400

    shiny app first attempt, vert layout

---
+++ littleNormalDist/app.R
@@ -0,0 +1,64 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("A Perfectly Normal Distribution"),
+   
+   #Get the parameters for our distribution
+   
+   #mu
+   fluidRow(
+            column(3, 
+            numericInput("mu", h3("Mean"), 
+            value = 0))
+   ),
+   
+   #sigma^2
+   fluidRow(
+            column(3, 
+            numericInput("sigma2", h3("Variance"), 
+            value = 1))
+   ),
+
+  #n
+  fluidRow(     
+           column(3, 
+           sliderInput("n", h3("Number of samples:"),
+           min = 0, 
+           max = 1000, 
+           value = 50))
+  ), 
+   
+   # Show a plot of the generated distribution
+   mainPanel(
+             plotOutput("distPlot")
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+     
+      # generate distribution based on input$mu, input$sigma2, and input$n from ui.R
+      x <- rnorm(1:input$n, input$mu, input$sigma2)
+      
+      # draw the histogram of values
+      hist(x, col = 4)
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
