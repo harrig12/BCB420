102b0e32b5eb32aed5883e6e3321c96873c8cc4a
Cait <cait.harrigan@mail.utoronto.ca>
2018-04-02 20:18:18 -0400

    BWT and back from string in file

--- BWT.R
+++ BWT.R
@@ -16,15 +16,70 @@
 # ==============================================================================
 
 if (!require(Biostrings, quietly=TRUE)) {
-  install.packages("Biostrings")
+  biocLite("Biostrings")
   library(Biostrings)
 }
 
-#load some gene of interest
-mySeq <- readDNAStringSet('data/EGFrefseq.fa')
+if (!require(seqinr, quietly=TRUE)) {
+  biocLite("seqinr")
+  library(seqinr)
+}
 
-#Select some query... the 10th line of the fasta file will do fine. (the 701-770th bases)
-myQuery <- 'CGATGGTGTGGGAGTGAAGGCTCTGTTGGAGACATCAGAGAAAATAACAGCTGTGTCATTGGATGTGCTT' 
+seqFile = 'data/seqBanana.fa'
+
+#load some sequence of interest (if fasta file)
+mySeq <- read.fasta(seqFile)
+mySeq <- unlist(mySeq)
+
+#load some sequence of interest (if txt file)
+# mySeq <- readLines(seqFile)
+
+#compute the suffix array (method from https://support.bioconductor.org/p/24049/ 
+#and in consultation of http://stanford.edu/class/cs262/notes/lecture4.pdf )
+mySA <- order(rank(mySeq, ties.method = 'last'))
+
+#We can construct BWT(X) from the suffix array S. At each position, take
+#the letter to the left of the one pointed to by S.
+# (from http://stanford.edu/class/cs262/notes/lecture4.pdf )
+
+#find BWT
+s2BWT <- function(s, suffixArray){ 
+  # s is a character array
+  # suffixArray gives the indices of the suffix array of s in order
+  stringBWT <- character()
+  for (i in suffixArray){
+    i <- i-1
+    if (i == 0){i <- max(suffixArray)}
+    stringBWT <- c(stringBWT, s[i])
+  }
+  return(stringBWT)
+}
+
+#We can reconstruct the original string from its BWT by repeatedly sorting a
+#list of strings then prepending the BWT.
+# (from http://stanford.edu/class/cs262/notes/lecture4.pdf)
+
+#build original string from BWT
+BWT2s <- function(sBWT){
+  
+  stringO <- character()
+  dfBWT <- as.data.frame(sBWT)
+  
+  for (i in 1:nrow(dfBWT)){
+    stringO <- paste0(dfBWT[,1], sort(stringO))
+    dfBWT <- cbind(sBWT, stringO)
+  }
+
+  return(as.character(dfBWT[which(dfBWT[,1] == "$") - 1, 2]))
+}
+
+#did we get the initial string back?
+stringI <- BWT2s(s2BWT(mySeq, mySA))
+
+(stringS == stringI) #True
+
+#search a substring
+
 
 
 # [END]
